0144cf4c569c9be7dd77e4f562e8fb7f3cf11e20
Neil Maldonado <neilmaldy@gmail.com>
2018-08-27 11:50:54 -0400

    starting excel output

--- psirt.py
+++ psirt.py
@@ -10,8 +10,8 @@ import pickle
 import xlsxwriter
 
 debug_it = 1
-output_file = 'advisories_' + datetime.date.today().strftime("%Y%U%w")
-
+output_file = 'advisories_' + time.strftime("%Y%m%d")
+# time.strftime("%Y%m%d%H%M%S")
 
 def print_to_log(log_string):
     """ Print to log file (stderr)
@@ -28,28 +28,34 @@ class ProductAdvisory:
 
     failed_attributes = []
 
-    def __init__(self, product_name, advisory):
+    def __init__(self, product_name, product_status, advisory):
         self.product = product_name
+        self.product_status = product_status
         self.changes = set()
         self.ntap_advisory_id = advisory['ntap_advisory_id']
         self.title = advisory['kb_title']
         self.summary = advisory['kb_summary']
-        self.cves = set()
+
+        self.cves_set = set()
         for cve in advisory['kb_scoring_calc']:
-            self.cves.add(cve + ' ' + advisory['kb_scoring_calc'][cve]['score']
+            self.cves_set.add(cve + ' ' + advisory['kb_scoring_calc'][cve]['score']
                           + ' ' + advisory['kb_scoring_calc'][cve]['range'])
-        self.fixes = set()
+        self.cves = ''
+        for cve in self.cves_set:
+            self.cves += cve + '\n'
+
+        self.fixes_set = set()
         for fix in advisory['kb_fixes']:
             if fix['product'] == product:
                 if 'fixes' in fix:
                     for fix_link in fix['fixes']:
                         if 'link' in fix_link:
-                            self.fixes.add(fix_link['link'])
+                            self.fixes_set.add(fix_link['link'])
                         else:
                             if fix['wontfix'] == 'true':
-                                self.fixes.add('WONT_FIX')
+                                self.fixes_set.add('WONT_FIX')
                             else:
-                                self.fixes.add('')
+                                self.fixes_set.add('')
 
     def list_changes(self, other):
         changes_list = []
@@ -94,7 +100,11 @@ for f in os.listdir('.'):
 advisory_table = {}
 for adv in advisories:
     for product in adv['kb_affected_list']:
-        advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, adv)
+        advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Affected', adv)
+    for product in adv['kb_investigating_list']:
+        advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Investigating', adv)
+    for product in adv['kb_unaffected_list']:
+        advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Unaffected', adv)
 
 with open(output_file + '.pickle', 'wb') as f:
     pickle.dump(advisory_table, f)
@@ -102,7 +112,7 @@ with open(output_file + '.pickle', 'wb') as f:
 days = 1
 previous_advisories = None
 while days < 90:
-    day = (datetime.date.today() - datetime.timedelta(days=days)).strftime("%Y%U%w")
+    day = (datetime.date.today() - datetime.timedelta(days=days)).strftime("%Y%m%d")
     if not os.path.isfile('advisories_' + day + '.pickle'):
         days += 1
         print_to_log("Comparing to history from " + day)
@@ -128,32 +138,40 @@ for key, product_advisory in advisory_table.items():
 
 column_list = [('Advisory ID', 'ntap_advisory_id', 25),
                ('Title', 'title', 40),
-               ('Product', 'product', 30)]
+               ('Product', 'product', 30),
+               ('CVEs', 'cves', 30)]
 
 workbook = xlsxwriter.Workbook(output_file + '.xlsx')
 worksheet = workbook.add_worksheet()
-fill_yellow_format = workbook.add_format()
-fill_yellow_format.set_bg_color('yellow')
+fill_yellow_format = workbook.add_format({'bg_color': 'yellow', 'text_wrap': True, 'border': 1})
+wrap_format = workbook.add_format({'text_wrap': True})
+heading_format = workbook.add_format({'font_color': 'white', 'bg_color': 'blue', 'bold': True, 'border': 1, 'align': 'center'})
+data_cell_format = workbook.add_format({'text_wrap': True, 'border': 1})
 
 row = 0
 col = 0
 
 for column_heading, attribute_name, column_width in column_list:
-    worksheet.write(row, col, column_heading)
-    worksheet.set_column(first_col=col, last_col=0, width=column_width)
+    worksheet.set_column(first_col=col, last_col=0, width=column_width, cell_format=wrap_format)
+    worksheet.write(row, col, column_heading, heading_format)
     col += 1
 
 row = 1
 col = 0
 for key in advisory_table:
     for column_heading, attribute_name, column_width in column_list:
-        if attribute_name in advisory_table[key].changes:
+        if attribute_name == 'ntap_advisory_id':
+            worksheet.write_url(row, col, 'https://security.netapp.com/advisory/' + advisory_table[key].__getattribute__(attribute_name).lower(), string=advisory_table[key].__getattribute__(attribute_name), cell_format=data_cell_format)
+        elif attribute_name in advisory_table[key].changes:
             worksheet.write(row, col, advisory_table[key].__getattribute__(attribute_name), fill_yellow_format)
         else:
-            worksheet.write(row, col, advisory_table[key].__getattribute__(attribute_name))
-
+            worksheet.write(row, col, advisory_table[key].__getattribute__(attribute_name), data_cell_format)
         col += 1
     row += 1
+    col = 0
+
+worksheet.autofilter(0, 0, len(advisory_table), len(column_list)-1)
 
 workbook.close()
+
 print('Done.')
