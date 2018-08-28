1aaa17811a6331d79f890a89caaae3ade0f5f6fb
Neil Maldonado <neilmaldy@gmail.com>
2018-08-28 12:46:11 -0400

    starting gui

--- psirt.py
+++ psirt.py
@@ -8,6 +8,35 @@ import sys
 import datetime
 import pickle
 import xlsxwriter
+import PySimpleGUI as sg
+import platform
+
+working_directory = None
+download_advisories = False
+
+while not working_directory:
+    with sg.FlexForm('NetApp Security Advisories Summary') as form:
+        form_rows = [[sg.Text('Choose working directory(required) and advisory.history file(optional)', size=(50, 1))],
+                     [sg.Text('(The advisory.history file is used to check for changes)', size=(50, 1))],
+                     [sg.Text('Working directory:', size=(14, 1)), sg.InputText(), sg.FolderBrowse()],
+                     [sg.Text('History file:', size=(14, 1)), sg.InputText(default_text='None'), sg.FileBrowse()],
+                     [sg.Checkbox('Download Advisories?', default=True)],
+                     [sg.Submit(), sg.Cancel()]]
+
+        button, values = form.LayoutAndShow(form_rows)
+
+    print(button, values)
+
+    if button is None or button == 'Cancel':
+        sys.exit()
+
+    working_directory, history_file, download_advisories = values
+
+    print(working_directory)
+    print(history_file)
+    print(download_advisories)
+
+os.chdir(working_directory)
 
 debug_it = 0
 output_file = 'advisories_' + time.strftime("%Y%m%d")
@@ -15,6 +44,7 @@ output_file = 'advisories_' + time.strftime("%Y%m%d")
 output_dir = 'AdvisoryDir'
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
+    download_advisories = True
 
 logs = []
 
@@ -28,6 +58,7 @@ def print_to_log(log_string):
     print(log, file=sys.stderr)
     logging.info(log)
     logs.append(log)
+    sg.EasyPrint(log, size=(80, 80))
 
     if debug_it:
         with open("temp.log", mode='a', encoding='utf-8') as templog:
@@ -48,7 +79,7 @@ class ProductAdvisory:
         self.first_fixed = ''
         self.notes = ''
         self.impact = advisory['kb_impact']
-        self.version = advisory['kb_rev_history'][-1]['version']
+        self.version = str(advisory['kb_rev_history'][-1]['version'])
         self.comment = advisory['kb_rev_history'][-1]['comment']
         self.date = advisory['kb_rev_history'][-1]['date']
         self.workarounds = advisory['kb_workarounds']
@@ -85,6 +116,7 @@ class ProductAdvisory:
                     if other.__getattribute__(k) != v:
                         changes_list.append((k, v, other.__getattribute__(k)))
                         self.changes.add(k)
+                        self.changes.add('ntap_advisory_id')
             except AttributeError:
                 if k not in ProductAdvisory.failed_attributes:
                     ProductAdvisory.failed_attributes.append(k)
@@ -98,11 +130,14 @@ response = requests.get(root_url)
 data = response.text
 soup = BeautifulSoup(data, 'lxml')
 
-if False:
+if download_advisories:
+    for f in os.listdir(output_dir):
+        if f.endswith('.json'):
+            os.remove(f)
     for link in soup.find_all('a'):
         if link.get('href').endswith('.json'):
             if debug_it or True:
-                print_to_log(link.get('href'))
+                print_to_log('Downloading ' + link.get('href'))
             adv = requests.get(root_url + link.get('href'))
             open(os.path.join(output_dir, link.get('href')), 'wb').write(adv.content)
 
@@ -113,16 +148,37 @@ for f in os.listdir(output_dir):
             advisories.append(json.loads(fp.read()))
 
 advisory_table = {}
+product_list = []
 for adv in advisories:
     for product in adv['kb_affected_list']:
         advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Affected', adv)
+        if product not in product_list:
+            product_list.append(product)
     for product in adv['kb_investigating_list']:
         advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Investigating', adv)
+        if product not in product_list:
+            product_list.append(product)
     # for product in adv['kb_unaffected_list']:
-    #     advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Unaffected', adv)
+    #    advisory_table[adv['ntap_advisory_id'] + ' ' + product] = ProductAdvisory(product, 'Unaffected', adv)
+    #    if product not in product_list:
+    #        product_list.append(product)
 
 with open(output_file + '.pickle', 'wb') as f:
     pickle.dump(advisory_table, f)
+
+with sg.FlexForm('NetApp Security Advisories Summary') as form:
+    form_rows = [[sg.Text('Choose products to include in report, select one item and then CTRL-A to select all', size=(80, 1))],
+                 [sg.Listbox(sorted(product_list), select_mode='multiple', size=(80, 40))],
+                 [sg.Submit(), sg.Cancel()]]
+
+    button, values = form.LayoutAndShow(form_rows)
+
+print(button, values)
+
+if button is None or button == 'Cancel':
+    sys.exit()
+
+product_list = values[0]
 
 days = 1
 previous_advisories = None
@@ -141,14 +197,14 @@ while days < 90:
 if previous_advisories:
     new_advisories = set(advisory_table.keys()) - set(previous_advisories.keys())
     for key in new_advisories:
-        if debug_it or True:
-            print_to_log('New advisory: ' + key)
+        print_to_log('New advisory: ' + key)
+        advisory_table[key].changes.add('ntap_advisory_id')
         advisory_table[key].changes.add('product')
     for key, product_advisory  in advisory_table.items():
         if key in previous_advisories:
             for change in product_advisory.list_changes(previous_advisories[key]):
                 if debug_it or True:
-                    print_to_log(key + str(change))
+                    print_to_log(key + ' Changes: ' + str(change))
 
 column_list = [('Advisory ID', 'ntap_advisory_id', 20),
                ('Title', 'title', 50),
@@ -165,7 +221,8 @@ column_list = [('Advisory ID', 'ntap_advisory_id', 20),
                ('Workarounds', 'workarounds', 26),
                ]
 
-workbook = xlsxwriter.Workbook(output_file + '.xlsx')
+time_stamp = time.strftime("%H%M%S")
+workbook = xlsxwriter.Workbook(output_file + time_stamp + '.xlsx')
 worksheet = workbook.add_worksheet('Advisories')
 fill_yellow_format = workbook.add_format({'bg_color': 'yellow', 'text_wrap': True, 'border': 1, 'font_size': 8})
 wrap_format = workbook.add_format({'text_wrap': True})
@@ -183,21 +240,22 @@ for column_heading, attribute_name, column_width in column_list:
 row = 1
 col = 0
 for key in advisory_table:
-    for column_heading, attribute_name, column_width in column_list:
-        if attribute_name == 'ntap_advisory_id' and 'ntap_advisory_id' in advisory_table[key].changes:
-            worksheet.write_url(row, col,
-                                'https://security.netapp.com/advisory/' + advisory_table[key].__getattribute__(
-                                    attribute_name).lower(),
-                                string=advisory_table[key].__getattribute__(attribute_name),
-                                cell_format=fill_yellow_format)
-        elif attribute_name == 'ntap_advisory_id':
-            worksheet.write_url(row, col, 'https://security.netapp.com/advisory/' + advisory_table[key].__getattribute__(attribute_name).lower(), string=advisory_table[key].__getattribute__(attribute_name), cell_format=data_cell_format)
-        elif attribute_name in advisory_table[key].changes:
-            worksheet.write_string(row, col, advisory_table[key].__getattribute__(attribute_name), fill_yellow_format)
-        else:
-            worksheet.write_string(row, col, advisory_table[key].__getattribute__(attribute_name), data_cell_format)
-        col += 1
-    row += 1
+    if advisory_table[key].product in product_list:
+        for column_heading, attribute_name, column_width in column_list:
+            if attribute_name == 'ntap_advisory_id' and 'ntap_advisory_id' in advisory_table[key].changes:
+                worksheet.write_url(row, col,
+                                    'https://security.netapp.com/advisory/' + advisory_table[key].__getattribute__(
+                                        attribute_name).lower(),
+                                    string=advisory_table[key].__getattribute__(attribute_name),
+                                    cell_format=fill_yellow_format)
+            elif attribute_name == 'ntap_advisory_id':
+                worksheet.write_url(row, col, 'https://security.netapp.com/advisory/' + advisory_table[key].__getattribute__(attribute_name).lower(), string=advisory_table[key].__getattribute__(attribute_name), cell_format=data_cell_format)
+            elif attribute_name in advisory_table[key].changes:
+                worksheet.write_string(row, col, str(advisory_table[key].__getattribute__(attribute_name)), fill_yellow_format)
+            else:
+                worksheet.write_string(row, col, str(advisory_table[key].__getattribute__(attribute_name)), data_cell_format)
+            col += 1
+        row += 1
     col = 0
 
 worksheet.autofilter(0, 0, len(advisory_table), len(column_list)-1)
@@ -211,4 +269,18 @@ for log in logs:
 
 workbook.close()
 
-print('Done.')
+with sg.FlexForm('NetApp Security Advisories Summary') as form:
+    form_rows = [[sg.Text('NetApp Security Advisories Summary saved as ' + output_file + time_stamp + '.xlsx', size=(60, 1))],
+                 [sg.Text('Open now?', size=(50, 1))],
+                 [sg.Ok(), sg.Cancel()]]
+
+    button, values = form.LayoutAndShow(form_rows)
+
+if button == 'Ok':
+    if platform.system() == "Darwin":
+        # mac
+        os.system("open " + output_file + time_stamp + '.xlsx.')
+    else:
+        # pc
+        os.system("start " + output_file + time_stamp + '.xlsx.')
+
